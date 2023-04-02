80dbb06f09e1fb8015038854b0d8ba9610c79c9a
adrianakatarina <dricakatarina@gmail.com>
2023-04-02 14:17:49 -0300

    posicionamento do card de post css

--- src/pages/feed/feed.js
+++ src/pages/feed/feed.js
@@ -20,20 +20,34 @@ export default () => {
     </section>
     <section class='post'>
       <img src='/image/user.svg' alt='user' class='img-user'>
-      <p></p>
-      <p><p>
-      <img src='/image/edit.svg' alt='edit' class='img-edit'>
-      <p><p>
-      <img src='/image/like.svg' alt='like' class='img-like'>
-      <img src='/image/comment.svg' alt='comentario' class='img-coment'>
-      <img src='/image/delete.svg' alt='delete' class='img-delete'>
+      <p class='username'>Nome Usuário</p>
+      <p class='hours'>7h<p>
+      <button class='img-edit'>
+        <img src='/image/edit.svg' alt='edit'>
+      </button>
+      <p class='message-post'>Oi! Hoje fiz uma torta de amora!</p>
+      <button class='img-like'>
+        <img src='/image/like.svg' alt='like' class='img-like'>
+      </button>
+      <button class='img-comment'>
+        <img src='/image/comment.svg' alt='comentario'>
+      </button>
+      <button class='img-delete'>
+        <img src='/image/delete.svg' alt='delete'>
+      </button>    
     </section>
   </section>
-  <footer class='nav-feed'>
-    <img src='/image/home.svg' alt='home' class='img-home'>
-    <img src='/image/hash.svg' alt='hash' class='img-hash'>
-    <img src='/image/logout.svg' alt='sair' class='img-logout'>
-  </footer>
+  <nav class='nav-feed'>
+    <a href="/#feed" class='img-home'>
+      <img src='/image/home.svg' alt='home'>
+    </a>
+    <a href="/#hash" class='img-hash'>
+      <img src='/image/hash.svg' alt='hash'>
+    </a>
+    <a href="/#logout" class='img-logout'>
+      <img src='/image/logout.svg' alt='sair'>
+    </a>
+  </nav>
   `;
 
   container.innerHTML = template;
