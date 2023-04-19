dfd71690c534a35392b90fdb789185146ad75f2b
Polyana <polyanacristina2001@gmail.com>
2023-04-19 17:33:08 -0300

    readme do projeto finalizado

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -26,13 +26,13 @@ export default () => {
       <p class='text-about'>Sobre as Desenvolvedoras:</p> 
       <section class='dev'>
         <img src='/image/dev-poly.png' alt='imagem poly' class='img-dev'>  
-        <p class='text-dev'>Me chamo Polyana Feitoza, tenho 21 anos e sou do Paraná. <br> Sempre foi apaixonada por doces <br> e o meu favorito é donnuts.</p> 
+        <p class='text-dev'>Me chamo Polyana Feitoza, tenho 21 anos e sou do Paraná. Sempre foi apaixonada por doces e o meu favorito é donnuts.</p> 
         <a class='icon-git' href="https://github.com/PolyanaCristinaFeitoza" target='_blank'> <img src='/image/icon-git.svg' alt='icone do gitHub' class='img-git'></a> 
         <a class='icon-linkedin' href="https://www.linkedin.com/in/polyftza/" target='_blank'> <img src='/image/icon-linkedin.svg' alt='icone do gitHub' class='img-linkedin'></a>
       </section>
       <section class='dev'>
         <img src='/image/dev-adri.png' alt='imagem adri' class='img-dev'>  
-        <p class='text-dev'>Me chamo Adriana Oliveira, tenho 26 anos e sou de Fortaleza. <br> Nãe consigo esperar até a festa junina para comer meu doce favorito que é o bolo de macaxeira.</p>
+        <p class='text-dev'>Me chamo Adriana Oliveira, tenho 26 anos e sou de Fortaleza. Nãe consigo esperar até a festa junina para comer meu doce favorito que é o bolo de macaxeira.</p>
         <a class='icon-git' href="https://github.com/AdrianaKatarina" target='_blank'> <img src='/image/icon-git.svg' alt='icone do gitHub' class='img-git'></a> 
         <a class='icon-linkedin' href="https://www.linkedin.com/in/adroliveira/" target='_blank'> <img src='/image/icon-linkedin.svg' alt='icone do gitHub' class='img-linkedin'></a>
       </section>
